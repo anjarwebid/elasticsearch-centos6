ecaa14194bc6f007aca592c00c1dcaa0e77cea6b
anjarwebid <info@anjar.web.id>
2014-08-14 18:47:05 +0700

    edit run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Start Elasticsearch
 echo "Starting Elasticsearch..."
-/usr/share/elasticsearch/bin/elasticsearch -d -Des.default.path.conf=/etc/elasticsearch
+/usr/share/elasticsearch/bin/elasticsearch -Des.default.path.conf=/etc/elasticsearch
